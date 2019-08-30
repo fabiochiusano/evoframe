4b9dd898672eeb8794245369f7df2ff91b9d4b39
fabiochiusano <chiusanofabio@hotmail.it>
2019-08-30 22:49:25 +0200

    Add bias to FNN

--- evoframe/models/feedforward_nn.py
+++ evoframe/models/feedforward_nn.py
@@ -1,5 +1,5 @@
 from evoframe.models.model import Model
-from evoframe.models.utils import mask_matrix
+from evoframe.models.utils import mask_tensor
 import numpy as np
 import pickle
 
@@ -13,7 +13,7 @@ class FeedForwardNetwork(Model):
             for index
             in range(len(layer_sizes)-1)]
         )
-        #self.biases = np.array([np.random.randn(ls) for ls in layer_sizes[1:]])
+        self.biases = np.array([np.random.randn(ls) for ls in layer_sizes[1:]])
         self.activations = activations
         self.last_activation = last_activation
 
@@ -25,30 +25,49 @@ class FeedForwardNetwork(Model):
                 out = self.activations(out)
             else:
                 out = self.last_activation(out)
+            out += self.biases[i]
         return out
 
     def es_1_copy(self):
         new_model = FeedForwardNetwork(self.layer_sizes, self.activations, self.last_activation)
         new_model.weights = self.weights
+        new_model.biases = self.biases
         return new_model
 
     def es_1_mutation(self, sigma=0.3, keep_perc=0.3):
         new_model = FeedForwardNetwork(self.layer_sizes, self.activations, self.last_activation)
-        x = []
+
+        new_weights = []
         for w in self.weights:
             noise = np.random.randn(*w.shape) * sigma
-            mask = mask_matrix(w, keep_perc=keep_perc)
-            x.append(w + noise * mask)
-        new_model.weights = np.array(x)
+            mask = mask_tensor(w, keep_perc=keep_perc)
+            new_weights.append(w + noise * mask)
+        new_model.weights = np.array(new_weights)
+
+        new_biases = []
+        for b in self.biases:
+            noise = np.random.randn(*b.shape) * sigma
+            mask = mask_tensor(b, keep_perc=keep_perc)
+            new_biases.append(b + noise * mask)
+        new_model.biases = np.array(new_biases)
+
         return new_model
 
     def es_2_crossover(self, other, keep_perc=0.8):
         new_model = FeedForwardNetwork(self.layer_sizes, self.activations, self.last_activation)
-        x = []
+
+        new_weights = []
         for i,w in enumerate(self.weights):
-            mask = mask_matrix(w, keep_perc=0.8)
-            x.append(w * mask + other.weights[i] * (1 - mask))
-        new_model.weights = np.array(x)
+            mask = mask_tensor(w, keep_perc=0.8)
+            new_weights.append(w * mask + other.weights[i] * (1 - mask))
+        new_model.weights = np.array(new_weights)
+
+        new_biases = []
+        for i,b in enumerate(self.biases):
+            mask = mask_tensor(b, keep_perc=0.8)
+            new_biases.append(b * mask + other.biases[i] * (1 - mask))
+        new_model.biases = np.array(new_biases)
+
         return new_model
 
     def save(self, filename='weights.pkl'):
--- evoframe/models/utils.py
+++ evoframe/models/utils.py
@@ -1,9 +1,9 @@
 import numpy as np
 
-def mask_matrix(matrix, keep_perc):
-    shape = matrix.shape
-    mask = np.full(shape[0] * shape[1], 0)
-    mask[:int(len(matrix) * keep_perc)] = 1
+def mask_tensor(tensor, keep_perc):
+    shape = tensor.shape
+    mask = np.full(np.prod(np.array(shape)), 0)
+    mask[:int(len(mask) * keep_perc)] = 1
     np.random.shuffle(mask)
     mask = np.reshape(mask, shape)
     return mask
